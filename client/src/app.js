53a52b03539d317f85338ef1a61cc05f3c1c6672
ckndy <chris.kennedy@live.co.uk>
2017-04-08 16:24:52 +0100

    added map

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,34 @@
 //this is app.js
 
+
+// Google Map Stuff
+
+var mapWrapper = function(container, center, zoom){
+  this.googleMap = new google.maps.Map(container, {
+    center: center,
+    zoom: zoom
+  });
+};
+
+MapWrapper.prototype = {
+  addMaker: function(coords){
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap
+    });
+
+    var app = function(){
+      var container = document.getElementById("GoogleMap");
+      var center = { lat: , lng:},
+    };
+
+    var zoom = 10;
+
+    var mainMap = new MapWrapper(container, center, zoom);
+
+
+    // ---------------------------------------------------------
+
 var UI = require('./views/ui');
 
 
