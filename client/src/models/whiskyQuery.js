b48880a923a88cb5de5a05835a8aa8a236ee2d52
Colin McKean <colinmckeandigital@gmail.com>
2017-04-08 19:38:29 +0100

    add all function to get docs from mongodb

--- client/src/models/whiskyQuery.js
+++ client/src/models/whiskyQuery.js
@@ -6,7 +6,16 @@ var WhiskyQuery = function(){
 };
 
 
-
+WhiskyQuery.prototype = {
+  all:function(onQueryFinished){
+    MongoClient.connect(this.url, function(err, db){
+      var collection = db.collection('whisky_distilleries');
+      collection.find().toArray(function(err, docs){
+        onQueryFinished(docs);
+      });
+    });
+  }
+}
 
 
 module.exports = WhiskyQuery;
