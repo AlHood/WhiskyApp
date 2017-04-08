6df6a191b293e9f74561310edc17f0c8ad5957b3
Colin McKean <colinmckeandigital@gmail.com>
2017-04-08 20:25:49 +0100

    add code to connect to db

--- controllers/locations.js
+++ controllers/locations.js
@@ -5,12 +5,17 @@ var express = require('express');
 var locationRouter = express.Router();
 var bodyParser = require('body-parser');
 
+var WhiskyQuery = require('../client/src/models/whiskyQuery');
+var query = new WhiskyQuery();
+
 
 locationRouter.use(bodyParser.json());
 locationRouter.use(bodyParser.urlencoded({extended: true}));
 
 locationRouter.get('/', function(req, res){
-  res.json(locations);
+  query.all(function(results){
+    res.json(results);
+  });
 });
 
 module.exports = locationRouter;
