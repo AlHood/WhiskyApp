f116ce220e033a7d781af0c2a2db2dece8b2352d
Colin McKean <colinmckeandigital@gmail.com>
2017-04-08 19:31:24 +0100

    inital layout of whisky query

--- client/src/models/whiskyQuery.js
+++ client/src/models/whiskyQuery.js
@@ -1,1 +1,12 @@
 //this is whiksyeQuery.js
+var MongoClient = require('mongodb').MongoClient;
+
+var WhiskyQuery = function(){
+  this.url = 'mongodb://localhost:27017/whisky';
+};
+
+
+
+
+
+module.exports = WhiskyQuery;
