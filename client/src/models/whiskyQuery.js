0e1c351d598e6df1a23c1aff24fa67452edac523
Colin McKean <colinmckeandigital@gmail.com>
2017-04-09 19:42:41 +0100

    add query to find location by object id

--- client/src/models/whiskyQuery.js
+++ client/src/models/whiskyQuery.js
@@ -1,17 +1,30 @@
 //this is whiksyeQuery.js
 var MongoClient = require('mongodb').MongoClient;
-
+//required in objectID to use findbyid.
+var ObjectID = require('mongodb').ObjectID;
 var WhiskyQuery = function(){
   this.url = 'mongodb://localhost:27017/whisky';
 };
 
 
 WhiskyQuery.prototype = {
+  //this is getting all locations 
   all:function(onQueryFinished){
     MongoClient.connect(this.url, function(err, db){
       var collection = db.collection('whisky_distilleries');
       collection.find().toArray(function(err, docs){
         onQueryFinished(docs);
+      });
+    });
+  },
+  //this is getting a location based on its object id, this may be refactored relater to use a place_id
+  findById: function(onQueryFinished, id) {
+    //logging out the id for now till front end is hooked up.
+    console.log({'_id':new ObjectID(id)})
+    MongoClient.connect(this.url, function(err, db){
+      var collection = db.collection('whisky_distilleries');
+      collection.find({'_id': new ObjectID(id)}).toArray(function(err, docs){
+        onQueryFinished(docs[0]);
       });
     });
   }
