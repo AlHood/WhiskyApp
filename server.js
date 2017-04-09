c00bc62a986ee918b429bb676ae1d738f8519c0b
glisterghost <glisterghost@gmail.com>
2017-04-09 14:15:33 +0100

    added html route to server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(require('./controllers'));
 
 app.use(express.static('client/build'));
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+});
 
 app.listen(3000, function () {
   console.log('App running on port '+this.address().port);
