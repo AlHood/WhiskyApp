32a63292e06e96b02b9996657b0109565bf517bc
glisterghost <glisterghost@gmail.com>
2017-04-10 14:14:51 +0100

    trying to populate the bucketlist in UI.js

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,1 +1,39 @@
 //this is ui.js
+
+var List = require('./models/user.js')
+
+
+
+var populateBucketList = function(){
+
+  var bucket_list = document.querySelector('#tabOne');
+   
+  for (distillery of ){
+    var name = document.createElement('p');
+    var region = document.createElement('p');
+    name.innerText = 
+    region.innerText = 
+
+    bucket_list.appendChild(name);
+    bucket_list.appendChild(region);
+
+
+    var button = document.createElement('button');
+    button.innerText = "DONE!"; 
+  }
+};
+
+
+var populateVisitedList function(){
+
+  var visited_list = document.querySelector('#tabTwo');
+    
+  
+}
+
+ var ui = function(){
+
+ }
+
+
+window.onload = ui;
